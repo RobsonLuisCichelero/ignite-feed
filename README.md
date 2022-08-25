a622bf192d856b5bd2e312528f321ec4e74fb0e9
Robson Luis Cichelero <robsonluiscicheleroo@gmail.com>
2022-08-25 20:01:35 -0300

    Projeto Final

---
+++ README.md
@@ -0,0 +1,29 @@
+# ignite-feed
+Project created during the Ignite, carried out by Rocketseat.
+
+### Technologies Used
+- React
+- Vite
+- Phosphoricons
+- date-fns
+
+## Application photos:
+ 
+<div>
+    <img src="./imageProject/project.png" width="99%" alt="">
+    <p style="display: flex; margin-top: 5px;" >
+        <img src="./imageProject/profile.png" width="50%" alt="">
+        <img src="./imageProject/feed.png" width="50%" alt="">
+    </p>
+</div>
+
+### How to start the project.
+
+run:
+```bash
+  yarn
+```
+and
+```bash
+  yarn dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import styles from './App.module.css';
+
+import './global.css';
+
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/RobsonLuisCichelero.png",
+      name: "Robson Cichelero",
+      role: "Frontend Develop"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋"},
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portif. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: 'link', content: "jane.design/doctorcaree"},
+    ],
+    publishedAt: new Date('2022-07-26 21:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋"},
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: 'link', content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date('2022-08-08 21:00:00')
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App;
